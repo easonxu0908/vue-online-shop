ed6ead73eceefed72e599aa36df1dd5315533269
eason <easonxu0908@gmail.com>
2019-05-02 14:39:07 +0800

    update

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue-online-shop/onlineshop/dist/index.html',
+    assetsPublicPath: '/vue-online-shop/dist/index.html',
 
     /**
      * Source Maps
